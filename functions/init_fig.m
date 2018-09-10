aa262d3abf7d50cf0752e6305f25f6240ed5eee3
Pengcheng Zhou <zhoupc1988@gmail.com>
2018-09-10 15:06:06 -0400

    add change figure location

--- functions/init_fig.m
+++ functions/init_fig.m
@@ -1,5 +1,12 @@
+if ~exist('fig_x0', 'var')
+    fig_x0 = 0;
+end
+if ~exist('fig_y0', 'var')
+    fig_y0 = 0;
+end
 set(gcf, 'color', 'w', ...
-    'defaultAxesFontSize', 20, ...) 
+    'defaultAxesFontSize', 20, ...)
     'defaultlinelinewidth',2, ...
-    'position', [0, 0, 100*get(gcf, 'papersize')], ...
-    'paperposition', [0, 0, get(gcf, 'papersize')])
+    'position', [fig_x0, fig_y0, 100*get(gcf, 'papersize')], ...
+    'paperposition', [0, 0, get(gcf, 'papersize')]); 
+clear fig_x0 fig_y0; 
