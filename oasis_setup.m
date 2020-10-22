6f42ebd0e2cbbbeeb2a73c537d2e02a54f5f34fc
ICBI <zhoupc@outlook.com>
2020-10-22 23:41:14 +0800

    avoid missing functions due to missing packages

--- oasis_setup.m
+++ oasis_setup.m
@@ -13,7 +13,7 @@ end
 
 %% install convex optimization solvers
 % by default, we don't install cvx any more. if you want to install cvx,
-% then set install_cvx = true and then run oasis-setup.m 
+% then set install_cvx = true and then run oasis-setup.m
 if exist('install_cvx', 'var') && install_cvx && isempty(which('cvx_begin.m'))
     if ~exist(fullfile(oasis_folder, 'packages', 'cvx'), 'dir')
         %install cvx
@@ -31,6 +31,19 @@ if exist('install_cvx', 'var') && install_cvx && isempty(which('cvx_begin.m'))
         unzip(cvx_url, fullfile(oasis_folder, 'packages'));
     end
     run(fullfile(oasis_folder, 'packages', 'cvx', 'cvx_setup.m'));
+end
+
+%% fix the bug if you don't have the following functions
+funs = {'pwelch'};
+flag = false;
+for m=1:length(funs)
+    if ~exist(funs{m}, 'file')
+        copyfile(fullfile(oasis_folder, 'missing_functions', funs{m}), ...
+            fullfile(oasis_folder, 'missing_functions', sprintf('%s.m', funs{m})));
+        if flag==false
+            addpath(fullfile(oasis_folder, 'missing_functions'));
+        end
+    end
 end
 %% save the current path
 %savepath();
